06f6212e7b2867e2f6e088f777f90b798f3986d9
thearmada <1622797605@qq.com>
2019-03-12 08:23:13 +0800

    反转一个整数

---
+++ reverse.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def reverse(self, x):
+        if -10<x<10:
+            return x
+        str_x = str(x)
+        if str_x[0] != "-":
+            str_x = str_x[::-1]
+            x = int(str_x)
+        else:
+            str_x = str_x[1:][::-1]
+            x = int(str_x)
+            x = -x
+        return x if -2147483648<x<2147483647 else 0
+if __name__ == '__main__':
+    s = Solution()
+    reverse_int = s.reverse(-120)
+    print(reverse_int)
